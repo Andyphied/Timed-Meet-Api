318432d22f793a8328daec61d067650e53c077a9
Philip Philip <philipandy2@gmail.com>
2020-10-19 18:23:54 +0100

    core of app added

---
+++ core/config.py
@@ -0,0 +1,55 @@
+import os
+from pathlib import Path
+from datetime import timedelta
+
+ENVIRON = os.environ.get("ENVIRON", "LOCAL")
+
+if ENVIRON == "LOCAL":
+    dir_path = Path(os.path.dirname(os.path.realpath(__file__)))
+    path = dir_path / ".."
+    MOCK_URI = "sqlite+pysqlite:///{file_path}"
+    FILE_PATH = f"{path}/db/db.sqlite3"
+    DB_URI = MOCK_URI.format(file_path=FILE_PATH)
+    DEBUG = True
+
+elif ENVIRON == "PRODUCTION":
+    db_params = {
+        "host": os.environ("HOST"),
+        "port": int(os.environ("PORT")),
+        "user": os.environ("USER"),
+        "pwd": os.environ("PWD"),
+        "db": os.environ("DB"),
+    }
+
+    DB_URI = "postgresql://{user}:{pwd}@{host}:{port}/{db}"
+    DB_URI = DB_URI.format(**db_params)
+    DEBUG = False
+
+
+class Config(object):
+    OPENAPI_VERSION = "3.0.2"
+    OPENAPI_JSON_PATH = "api-spec.json"
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_REDOC_PATH = "/redoc"
+    OPENAPI_REDOC_URL = (
+        "https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js")
+    OPENAPI_SWAGGER_UI_PATH = "/swagger-ui"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    SQLALCHEMY_DATABASE_URI = DB_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = DEBUG
+    JWT_TOKEN_LOCATION = ('headers', 'cookies')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=2)
+    JWT_IDENTITY_CLAIM = 'sub'
+    JWT_BLACKLIST_ENABLED = True
+
+
+class Setting():
+    API_TITLE = os.environ.get("API_TITLE", "MEET API")
+    API_VERSION = os.environ.get("API_VERSION", "0.1")
+    JWT_SECRET_KEY: str = os.environ.get('PRIVATE_KEY',
+                                         'OS0ceYEcMyZ9fqtCZO0F9A')
+
+
+setting = Setting()
---
+++ core/factory.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from core.config import Config, setting
+from flask_migrate import Migrate, MigrateCommand
+from flask_jwt_extended import JWTManager
+from flask_smorest import Api
+
+migrate = Migrate()
+jwt = JWTManager()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    app.config["API_TITLE"] = setting.API_TITLE
+    app.config["API_VERSION"] = setting.API_VERSION
+    app.config["JWT_SECRET_KEY"] = setting.JWT_SECRET_KEY
+    api = Api(app)
+
+    from models import db
+
+    db.init_app(app)
+    app.db = db
+
+    migrate.init_app(app, db)
+    app.cli.add_command(MigrateCommand, name="db")
+
+    jwt.init_app(app)
+
+    from schema import ma
+
+    ma.init_app(app)
+
+    from api import user_blp, auth_blp
+
+    api.register_blueprint(user_blp)
+    api.register_blueprint(auth_blp)
+
+    return app
